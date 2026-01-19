17f6b547324aca942560d58c63be5f6a1449b223
SnowvyTH <supanat617@gmail.com>
2026-01-19 22:51:15 +0700

    Day 2.2

--- TypeScriptDay2/src/app.ts
+++ TypeScriptDay2/src/app.ts
@@ -1,22 +1,25 @@
 // การ import แบบปกติผ่านชื่อ module ที่ต้องการ
-import { message as msg, messageTH, hello, welcome as wel } from './topics/hello'
+// import { message as msg, messageTH, hello, welcome as wel } from './topics/hello'
 
-// import logMessage from "./topics/logger";
-// การ import สามารถเป็นชื่ออะไรก็ได้
-import logs from './topics/logger'
-// import { default as logs } from './topics/logger'
-// import ทุกอย่างเป็น object
-import * as hey from './topics/hello'
+// // import logMessage from "./topics/logger";
+// // การ import สามารถเป็นชื่ออะไรก็ได้
+// import logs from './topics/logger'
+// // import { default as logs } from './topics/logger'
+// // import ทุกอย่างเป็น object
+// import * as hey from './topics/hello'
 
-// // เรียกใช้งานตัวแปรและฟังก์ชั่นที่ import มา
-console.log(msg)
-// console.log(messageTH)
-// hello()
-// welcome()
-wel()
+// // // เรียกใช้งานตัวแปรและฟังก์ชั่นที่ import มา
+// console.log(msg)
+// // console.log(messageTH)
+// // hello()
+// // welcome()
+// wel()
 
-logs("222")
-console.log(hey.message)
-console.log(hey.hello)
-hey.hello()
-hey.welcome()
+// logs("222")
+// console.log(hey.message)
+// console.log(hey.hello)
+// hey.hello()
+// hey.welcome()
+
+import { variabletype } from './topics/variable_type'
+variabletype()
